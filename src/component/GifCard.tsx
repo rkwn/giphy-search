92158eb44853211daf017c36289f1303a5da1dca
Raymond Kwan <kwanvm@gmail.com>
2021-10-26 01:13:21 -0400

    added favorite icon

--- src/component/GifCard.tsx
+++ src/component/GifCard.tsx
@@ -1,5 +1,5 @@
 import { GifObject } from '../api/giphy-types';
-
+import starRegular from '../assets/star-regular.svg';
 interface GifCardProps {
   gif: GifObject;
 }
@@ -11,8 +11,13 @@ const GifCard = ({ gif }: GifCardProps) => {
         src={gif.images.fixed_width_downsampled.url}
         alt="gif"
       />
-      {/* <div className="gif-info">{gif.title}</div> */}
-      <span className="gif-favorite-icon">⭐</span>
+      <img
+        className="gif-favorite-icon"
+        src={starRegular}
+        alt={'favorite icon'}
+        onClick={() => console.log('click')}
+      />
+      <div className="gif-info">{gif.title}</div>
     </div>
   );
 };
